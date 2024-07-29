a00558d887ad00fc80dede48590774cc82ad6851
Hamish Peebles <hamishpeebles@gmail.com>
2024-07-29 23:49:32 +0100

    Avoid having to specify canister Ids in init args (#16)

--- canister/src/lifecycle/init.rs
+++ canister/src/lifecycle/init.rs
@@ -4,7 +4,7 @@ use ic_cdk::init;
 
 #[init]
 fn init(args: InitOrUpgradeArgs) {
-    let init_args = args.to_init_args();
+    let init_args = args.into_init_args();
 
     let state = State::new(init_args);
     crate::jobs::start_jobs(&state);
