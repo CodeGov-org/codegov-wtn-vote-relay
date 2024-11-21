e32b298072736c3e93bfb908ac832819219a8962
Hamish Peebles <hamishpeebles@gmail.com>
2024-11-21 22:34:44 +0000

    Enforce a limit of 100 registrations

--- canister/src/queries/list_neuron_pairs.rs
+++ canister/src/queries/list_neuron_pairs.rs
@@ -3,5 +3,5 @@ use ic_cdk::query;
 
 #[query]
 fn list_neuron_pairs() -> Vec<NeuronPairPublic> {
-    state::read(|s| s.list_neuron_pairs())
+    state::read(|s| s.neuron_pairs().values().map(|pair| pair.into()).collect())
 }
