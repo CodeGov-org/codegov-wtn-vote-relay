a997e03df40091bdf5ad47de3a3e17221457b674
Hamish Peebles <hamishpeebles@gmail.com>
2024-12-28 09:53:21 +0000

    Make canister log only contain logs of when canister traps (#40)

--- canister/src/logs.rs
+++ canister/src/logs.rs
@@ -12,8 +12,6 @@ pub fn init(logs: Vec<String>) {
 pub fn log<S: AsRef<str>>(s: S) {
     let message = s.as_ref();
 
-    ic_cdk::println!("{message}");
-
     LOGS.with_borrow_mut(|logs| {
         let now = ic_cdk::api::time() / 1_000_000;
         logs.push_back(format!("{now}: {message}"));
